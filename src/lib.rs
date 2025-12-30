9538a4b7bbc401cc3bc69b80c5757adf1a391d15
张扬发 <zhangyangfa@zhangyangfadeMacBook-Pro.local>
2025-12-30 18:55:21 +0800

    little change

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,7 @@ use pyo3::prelude::*;
 use pyo3::exceptions::PyRuntimeError;
 use pyo3::types::PyModule;
 
-pub mod base;
-
-pub mod alpha;
-pub mod backtest;
-pub mod data;
-pub mod report;
+mod operators;
 
 #[pymodule]
 mod tomica(m: &Bound<'_, PyModule>) -> PyResult<()> {
