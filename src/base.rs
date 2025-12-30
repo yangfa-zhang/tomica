801e4cc57ea69e047559c76103d0e77f6535362f
张扬发 <zhangyangfa@zhangyangfadeMacBook-Pro.local>
2025-12-30 18:32:41 +0800

    simplified the repo

--- src/base.rs
+++
@@ -1,8 +0,0 @@
-use pyo3::prelude::*;
-use pyo3::exceptions::PyRuntimeError;
-use pyo3::types::PyModule;
-
-pub trait Model {
-    fn train(&mut self, x: Vec<Vec<f32>>, y: Vec<f32>, epochs: usize) -> PyResult<()>;
-    fn evaluate(&self, x: Vec<Vec<f32>>, y: Vec<f32>) -> PyResult<()>;
-}
