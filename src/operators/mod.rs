4d2f435d34df38667b476985ab4028e1f1240cd4
yangfazhang <yangfa1027@gmail.com>
2026-01-05 16:39:02 +0800

    add ts_sum

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -18,9 +18,9 @@ fn ts_delta(
     data: PySeries,
     n: i64,
 ) -> PyResult<PySeries> {
-    let out: Series = data.into();
-    let delayed_data = out.shift(n);
-    let delta = (&out - &delayed_data).map_err(|e| {
+    let data: Series = data.into();
+    let delayed_data = data.shift(n);
+    let delta = (&data - &delayed_data).map_err(|e| {
         PyRuntimeError::new_err(format!("Polars error: {}", e))
     })?;
     Ok(PySeries(delta))
@@ -31,9 +31,9 @@ fn ts_returns(
     data: PySeries,
     n: i64,
 ) -> PyResult<PySeries> {
-    let out: Series = data.into();
-    let delayed_data = out.shift(n);
-    let delta = (&out - &delayed_data).map_err(|e| {
+    let data: Series = data.into();
+    let delayed_data = data.shift(n);
+    let delta = (&data - &delayed_data).map_err(|e| {
         PyRuntimeError::new_err(format!("Polars error: {}", e))
     })?;
 
@@ -43,10 +43,28 @@ fn ts_returns(
     Ok(PySeries(returns))
 }
 
+#[pyfunction]
+fn ts_sum(
+    data: PySeries,
+    n: usize,
+) -> PyResult<PySeries>{
+    let data: Series = data.into();
+    let sum_series = data.rolling_sum(RollingOptionsFixedWindow {
+        window_size: n,
+        min_periods: 1,   
+        center: false,
+        ..Default::default()
+    }).map_err(|e| {
+        PyRuntimeError::new_err(format!("Polars error: {}", e))
+    })?;
+    Ok(PySeries(sum_series))
+}
+
 #[pymodule]
 pub fn operators(m: &Bound<'_, PyModule>)-> PyResult<()>{
     m.add_function(wrap_pyfunction!(ts_delay, m)?)?;
     m.add_function(wrap_pyfunction!(ts_delta, m)?)?;    
     m.add_function(wrap_pyfunction!(ts_returns, m)?)?;    
+    m.add_function(wrap_pyfunction!(ts_sum, m)?)?;    
     Ok(())
 }
