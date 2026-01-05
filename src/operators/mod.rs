633739c30ff9a269e8e0934fa9a551eef4ced9d7
yangfazhang <yangfa1027@gmail.com>
2026-01-05 16:20:57 +0800

    add ts_returns

--- src/operators/mod.rs
+++ src/operators/mod.rs
@@ -26,9 +26,27 @@ fn ts_delta(
     Ok(PySeries(delta))
 }
 
+#[pyfunction]
+fn ts_returns(
+    data: PySeries,
+    n: i64,
+) -> PyResult<PySeries> {
+    let out: Series = data.into();
+    let delayed_data = out.shift(n);
+    let delta = (&out - &delayed_data).map_err(|e| {
+        PyRuntimeError::new_err(format!("Polars error: {}", e))
+    })?;
+
+    let returns = (&delta / &delayed_data).map_err(|e| {
+        PyRuntimeError::new_err(format!("Polars error: {}", e))
+    })?;
+    Ok(PySeries(returns))
+}
+
 #[pymodule]
 pub fn operators(m: &Bound<'_, PyModule>)-> PyResult<()>{
     m.add_function(wrap_pyfunction!(ts_delay, m)?)?;
     m.add_function(wrap_pyfunction!(ts_delta, m)?)?;    
+    m.add_function(wrap_pyfunction!(ts_returns, m)?)?;    
     Ok(())
 }
