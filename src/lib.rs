849198c5a285d2e72827b21ac80ed5ca781eadc6
yangfazhang <yangfa1027@gmail.com>
2026-01-05 15:00:32 +0800

    first simple try

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,20 @@
 #![allow(non_snake_case)]
 use pyo3::prelude::*;
-use pyo3::exceptions::PyRuntimeError;
-use pyo3::types::PyModule;
+use pyo3_polars::types::PySeries;
+use polars::prelude::*;
 
-mod operators;
+#[pyfunction]
+fn ts_delay(
+    data: PySeries,
+    n: i64,
+) -> PyResult<PySeries> {
+    let out: Series = data.into();
+    let out = out.shift(n);
+    Ok(PySeries(out))
+}
 
 #[pymodule]
-mod tomica(m: &Bound<'_, PyModule>) -> PyResult<()> {
+fn tomica(m: &Bound<'_, PyModule>) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(ts_delay, m)?)?;
     Ok(())
 }
---
+++ src/operators/mod.rs
@@ -0,0 +1,4 @@
+use pyo3::prelude::*;
+use pyo3::exceptions::PyRuntimeError;
+use pyo3::types::PyModule;
+
